5534b5d0a21ceb11c9475e0418bcf41f93f20bc1
Mateus_Mota <mateusmota0100@gmail.com>
2022-01-13 00:45:09 -0300

    Atualizações no código principal.

--- portscan.py
+++ portscan.py
@@ -1,15 +1,23 @@
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.settimeout(0.05)
-
 ip = input('Digite o Host a ser verificado: ')
-port = int(input('Digite a Porta a ser verificado: '))
 
-code = client.connect_ex((ip, port))
+ports = []
+count = 0
+
+while count < 10:
+    ports.append(int(input('Digite a Porta a ser verificado: ')))
+    count += 1
+
+for port in ports:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(0.05)
+    code = client.connect_ex((ip, port))
 
 
-if code == 0:
-    print('Porta Aberta')
-else:
-    print('Porta fechada')
+    if code == 0:
+        print(f'{str(port)} -> Porta Aberta')
+    else:
+        print(f'{str(port)} -> Porta Fechada')
+        
+print('Scan Finalizado')
